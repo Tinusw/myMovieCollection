fe15c1663d27020c4daee24dd55577d4f1490a80
Tinus Wagner <tinus@3months.com>
2017-06-13 15:43:44 +1200

    moving slider component into form, showing image preview on add, and styling sliders after submit

--- src/components/movie/MoviePage.js
+++ src/components/movie/MoviePage.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { connect } from 'react-redux'
 import * as movieActions from '../../actions/movieActions'
 import FileBase64 from 'react-file-base64'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import './moviePage.css'
 
 class Movie extends React.Component{
@@ -12,12 +15,19 @@ class Movie extends React.Component{
     }
   }
 
+
   submitMovie(input){
     this.props.createMovie(input)
   }
 
+  // GetFiles from FileBase64 uploader and set to state
   getFiles(files){
     this.setState({ files: files })
+  }
+
+  // Reset state after submit
+  resetState(){
+    this.setState({ files: [] })
   }
 
   render () {
@@ -25,6 +35,12 @@ class Movie extends React.Component{
     let director
     let genre
     let description
+    let settings = {
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      autoplay: 500
+    }
 
     return (
       <div className='container'>
@@ -36,10 +52,12 @@ class Movie extends React.Component{
               <h4>{b.director}</h4>
               <h4>{b.genre}</h4>
               <h4>{b.description}</h4>
-              <div className="col-lg-12">
-                {b.images.map((b, i) =>
-                  <img key={i} src={b.base64} className="img-fluid" alt="responsive-image"></img>
-                )}
+              <div className="col-lg-12 image-slider">
+                <Slider {...settings}>
+                  {b.images.map((b, i) =>
+                    <div><img key={i} src={b.base64}></img></div>
+                  )}
+                </Slider>
               </div>
             </div>
           )}
@@ -60,20 +78,26 @@ class Movie extends React.Component{
             if (!description.value.trim()){
               return
             }
-            // Set our form values as input object
+            // Set our form values as an object
             var input ={title: title.value, director: director.value, genre: genre.value, description: description.value, images: this.state.files}
             this.submitMovie(input)
             e.target.reset();
+            this.resetState()
           }}>
             <input type="text" name="title" ref={node => title = node}/>
             <input type="text" name="director" ref={node => director = node}/>
             <input type="text" name="genre" ref={node => genre = node}/>
             <input type="text" name="description" ref={node => description = node}/>
+            <FileBase64
+              multiple={ true }
+              onDone={ this.getFiles.bind(this) } />
+            <div className="col-lg-3">
+              {this.state.files.map((b, i) =>
+                <img src={b.base64}></img>
+              )}
+            </div>
             <input type="submit"/>
           </form>
-          <FileBase64
-            multiple={ true }
-            onDone={ this.getFiles.bind(this) } />
         </div>
       </div>
     )
